c81b3dc873a89d58693a36826029d9a171df7557
erinmlee97 <erinmlee97@gmailcom>
2020-09-23 16:14:32 -0400

    added data to html

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -4,3 +4,18 @@ console.group(tableData);
 
 //Reference table body
 var tbody = d3.select("tbody");
+
+// UFO Sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    // Append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+
+    // Console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
