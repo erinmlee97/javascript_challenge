2761ef2ee2417558894f9a390e9b8de8472f6f3f
erinmlee97 <erinmlee97@gmailcom>
2020-09-23 16:07:54 -0400

    finished level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -12,7 +12,7 @@ tableData.forEach(function(ufoSighting) {
     // Append one table row `tr` for each UFO Sighting object
     var row = tbody.append("tr");
 
-    // Use `Object.entries` to console.log each UFO Sighting value
+    // Console.log each UFO Sighting value
     Object.entries(ufoSighting).forEach(function([key, value]) {
       console.log(key, value);
       // Append a cell to the row for each value
@@ -20,3 +20,36 @@ tableData.forEach(function(ufoSighting) {
       cell.text(value);
     });
   });
+
+// Create an event for button
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    tbody.html("");
+
+    // Select the input date
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input date, state, shape 
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    // Filter data to match input value 
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    console.log(filteredData);
+
+
+    filteredData.forEach(function(selections) {
+    console.log(selections);
+
+    // Append rows for each row in data
+    var row = tbody.append("tr");
+
+    // Console.log each UFO Sighting value
+    Object.entries(selections).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
